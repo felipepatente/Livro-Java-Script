8f55bff2c7a4058695b65e479fb75576ebfb20a4
felipepatente <felipepatente@yahoo.com.br>
2019-09-09 16:20:10 -0300

    Fazendo as validações para os campos de text e select

--- Capítulo 13 - Validando Formulários com JavaScript/validacao_form.js
+++ Capítulo 13 - Validando Formulários com JavaScript/validacao_form.js
@@ -20,24 +20,38 @@ function captura_eventos(objeto, evento, funcao) {
 
 }
 
-function formValid(event) {
-	
-	if(document.forms[0].txtname.value.length == 0){
-		alert("O campo Nome é obrigatório");
-
-		if(event.preventDefaut){
-			event.preventDefaut();
-		}else{
-			window.event.returnValue = false;
-		}
-
-		return false;
+function cancela_evento(evento) {
+	if(event.preventDefaut){
+		event.preventDefaut();
 	}else{
-		alert("Olá " + document.forms[0].txtname.value);
-		return true;
+		window.event.returnValue = false;
 	}
 }
 
+function formValid(event) {
+	
+	//pega os campos text e select
+	var campoNome = formE1.txtname.value,
+		campoCidade = formE1.cidades;	
 
+	//Verifica campo de texto
+	if(campoNome.length == 0){
+		alert("O campo Nome é obrigatório");
+		return false;
+	}
 
+	//Laço que percorre todas as opções
+	for(var i = 0; i < campoCidade.length; i++){
 
+		if(campoCidade[i].selected){
+			if(campoCidade[i].value == ""){
+				alert("Selecione uma opção.");
+				cancela_evento(evento);
+			}
+		}
+
+	}
+
+	alert("O Formulário será enviado");
+	return true;
+}
